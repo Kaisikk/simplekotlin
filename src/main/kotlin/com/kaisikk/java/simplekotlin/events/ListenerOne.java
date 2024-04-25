73da3c5ae6a4f431d98cdb44fa095c67350c9c05
Kaisikk <Kosikov.Ilya@mail.ru>
2024-04-26 00:08:51 +0400

    Работа с eventListener

--- src/main/kotlin/com/kaisikk/java/simplekotlin/events/ListenerOne.java
+++ src/main/kotlin/com/kaisikk/java/simplekotlin/events/ListenerOne.java
@@ -1,6 +1,5 @@
 package com.kaisikk.java.simplekotlin.events;
 
-import lombok.var;
 import org.springframework.context.ApplicationListener;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -8,7 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ListenerOne implements ApplicationListener<ImportEvent> {
     @Override
     public void onApplicationEvent(ImportEvent event) {
-        var timeout = ThreadLocalRandom.current().nextLong(5000L, 20000L);
+        long timeout = ThreadLocalRandom.current().nextLong(5000L, 20000L);
         System.out.println(Thread.currentThread().getName() + " " + timeout);
         try {
             Thread.sleep(timeout);
